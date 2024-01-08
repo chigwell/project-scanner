7081d6235c4c6968d46e1c891c318f4007c98204
Eugene Evstafiev <36392751+chigwell@users.noreply.github.com>
2024-01-08 17:48:09 +0000

    Add __init__.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='project-scanner',
-    version='0.1.0',
+    version='0.1.1',
     description='A Python package to scan project directories and files batch by batch.',
     long_description=long_description,
     long_description_content_type='text/markdown',
